4d0ef816a6e62a6d0cb99d9154be3dbcecb9c1c6
Stefan8609 <sk8609@princeton.edu>
2024-11-19 13:01:10 -0500

    add: data_cleaning

---
+++ src/apc524-election2024/data_cleaning.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+df = pd.read_csv("data/president_polls.csv")
+
+df = df.drop(
+    columns=[
+        "question_id",
+        "poll_id",
+        "cycle",
+        "pollster_id",
+        "sponsors",
+        "sponsor_ids",
+        "display_name",
+        "pollster_rating_id",
+        "pollster_rating_name",
+        "population",
+        "population_full",
+        "methodology",
+        "office_type",
+        "seat_number",
+        "seat_name",
+        "sponsor_candidate_id",
+        "sponsor_candidate",
+        "internal",
+        "partisan",
+        "tracking",
+        "nationwide_batch",
+        "created_at",
+        "notes",
+        "url",
+        "url_article",
+        "url_topline",
+        "url_crosstab",
+        "subpopulation",
+        "numeric_grade",
+        "pollscore",
+        "transparency_score",
+        "sponsor_candidate_party",
+        "endorsed_candidate_id",
+        "endorsed_candidate_name",
+        "endorsed_candidate_party",
+        "source",
+        "race_id",
+        "election_date",
+        "stage",
+        "ranked_choice_reallocated",
+        "ranked_choice_round",
+        "hypothetical",
+        "candidate_id",
+    ]
+)
+
+print(df)
+
+df.to_csv("data/president_polls_cleaned.csv", index=False)
