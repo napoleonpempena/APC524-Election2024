15e994685f05c7e1ae1ea4e2c0b4e2aede80e7b5
Gabriel Rios <mr.gabrielrios@gmail.com>
2024-12-13 15:20:38 -0500

    [fix] improved color assignment.

--- src/apc524-election2024/election_dashboard.py
+++ src/apc524-election2024/election_dashboard.py
@@ -150,10 +150,9 @@ def update_figure(candidate_names: dcc.Input,
             composite_monthly_average_reindexed = composite_monthly_average.reindex(months, level=1).ffill()
             composite_monthly_average_reindexed = composite_monthly_average_reindexed.reset_index(level=0).reset_index()
         else:
-            composite_monthly_average = input_dataframe.resample('W', on='start_date')[['dem_model_prediction', 'rep_model_prediction']].mean().dropna().reset_index()
-            months = pd.date_range(start=input_dataframe['start_date'].min(), end=input_dataframe['start_date'].max(), freq='W')
-            # composite_monthly_average_reindexed = composite_monthly_average.reset_index()
-            composite_monthly_average_reindexed = composite_monthly_average
+            composite_monthly_average = input_dataframe.resample('M', on='start_date')[['dem_model_prediction', 'rep_model_prediction']].mean().dropna()
+            months = pd.date_range(start=input_dataframe['start_date'].min(), end=input_dataframe['start_date'].max(), freq='M')
+            composite_monthly_average_reindexed = composite_monthly_average.reindex(months).ffill()
         return composite_monthly_average_reindexed
 
     ''' Calculate composite monthly averages. '''
