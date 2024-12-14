9e80e2796fbf0def4103a0bab0b046f570a15c88
Napoleon Pempena <napoleon@princeton.edu>
2024-12-13 19:16:51 -0500

    add src to import statement

--- tests/test_date_data.py
+++ tests/test_date_data.py
@@ -6,7 +6,7 @@ import pandas as pd
 # Add the src directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/apc524_election2024')))
 
-from apc524_election2024.date_data import get_date_data
+from src.apc524_election2024.date_data import get_date_data
 
 @pytest.fixture
 def sample_data():
